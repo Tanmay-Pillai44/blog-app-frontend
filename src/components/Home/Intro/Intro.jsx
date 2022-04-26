24c92195bc08aff962c74912cef04f8d50d8877b
Tanmay_Pillai <omtej4499@gmail.com>
2022-04-26 21:36:28 +0530

    Major Working Done

--- src/components/Home/Intro/Intro.jsx
+++ src/components/Home/Intro/Intro.jsx
@@ -9,12 +9,12 @@ const Intro = () => {
     return (
         <div className='Intro'>
             {
-                BlogData.filter((item) => item.id === 20).map((data, index) => (
+                BlogData.filter((item) => item.id === 37).map((data, index) => (
                     <IntroComponent data={data} classStyle='classStyle1' key={index} />
                 ))
             }
             {
-                BlogData.slice(26, 28).map((data, index) => (
+                BlogData.slice(8, 10).map((data, index) => (
                     <IntroComponent data={data} classStyle='classStyle2' key={index} />
                 ))
             }
--- src/components/PostPage/RelatedReadComponent.jsx
+++ src/components/PostPage/RelatedReadComponent.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import AuthorDetailsComponent from './AuthorDetailsComponent';
 
 const RelatedReadComponent = ({ data }) => {
 
-    const navigate =useNavigate();
-
-    const clickHandler = () => {
-        navigate(-1);
+    const handleClick = () => {
+        window.scrollTo(0, 0);
     }
 
     return (
         <div className='RelatedReadComponent' key={data.id}>
             <div className='related-reads-text'>Related reads</div>
             <img src={data.img} alt="related-read-img" className='related-read-img' />
-            <Link onClick={clickHandler} to={`${data.category}/${data.id}`}>
+            <Link onClick={handleClick} to={`/${data.category}/${data.id}`}>
                 <div className='related-read-title'>
-                    {data.title}
+                    {data.title.substring(0, 60)+"..."}
                 </div>
             </Link>
             <AuthorDetailsComponent data={data} classStyle='written-by-hide' />
